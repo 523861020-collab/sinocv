f95fc9d1d7f80ae07825ecd4a52028695fd037a0
lishanlong1988 <13001977959@163.com>
2026-07-07 17:44:31 +0800

    global: py-48 everywhere, line-height 1.8, section spacing

--- src/components/Advantages.tsx
+++ src/components/Advantages.tsx
@@ -29,7 +29,7 @@ const iconMap: Record<string, React.ReactNode> = {
 
 export default function Advantages() {
   return (
-    <section className="bg-black py-40">
+    <section className="bg-black py-48">
       <div className="container mx-auto px-6">
         {/* Section Header */}
         <motion.div
